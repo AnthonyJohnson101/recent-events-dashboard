63e104df2816b84c1b67299ca0165f7169b7a404
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-01-12 17:38:43 -0500

    added some modular functions to manipulate html text

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,14 @@
 
 
 //Stock API setup
-let ticker = 'AAPL' //make input
 
+//set to querey selector
+let closePrice
+let openPrice
+//pulls stock data and applies the text to its selectors
 function getStockData() {
+
+    let ticker = 'AAPL' //make input
 
     let stockUrl = `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?adjusted=true&apiKey=ZIxeNYptpyEx7ttuoFfptrcihI53Y_lo`
         fetch(stockUrl)
@@ -19,13 +24,36 @@ function getStockData() {
 
                 .then(function (data) {
                     console.log(data);
+
+                     /* openPrice.textContent = data.results[0].o
+                     closePrice.textContent = data.results[0].c */
                 });
             };
 
+           
+
             getStockData();
+
+            
 
 
 //News API setup
+
+//set to selectors
+let thumbnail1
+let title1
+let articleUrl1
+
+let thumbnail2
+let title2
+let articleUrl2
+
+let thumbnail3
+let title3
+let articleUrl3
+
+
+
 function getNewsData() {
 
     let now = dayjs().format("YYYY-MM-DD")
@@ -41,7 +69,75 @@ function getNewsData() {
 
                 .then(function (data) {
                     console.log(data);
+
+                    /* thumbnail1.src = data.articles[0].urlToImage
+                    title1.textContent = data.articles[0].title
+                    articleUrl1.href = data.articles[0].url
+
+                    thumbnail2.src = data.articles[1].urlToImage
+                    title2.textContent = data.articles[1].title
+                    articleUrl2.href = data.articles[1].url
+
+                    thumbnail3.src = data.articles[2].urlToImage
+                    title3.textContent = data.articles[2].title
+                    articleUrl3.href = data.articles[2].url */
+
+
                 });
             };
 
             getNewsData();
+
+
+
+
+//weather API setup
+let temp
+let icon
+function getWeatherData() {
+    let cityName = "Detroit"
+
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=536f859786a518738a9b20276be37ff1&units=imperial`
+
+            fetch(weatherUrl)
+            .then(function (response) {
+                
+                return response.json();
+            })
+
+            .then(function (data) {
+                console.log(data);
+
+                /* temp = data.list[0].main.temp
+                icon.src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png" */
+
+
+            });
+        };
+
+        getWeatherData();
+
+//dictionary API setup
+
+let word
+function getDictionaryData() {
+    let word = "purple"
+
+    let dictionaryUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3f822b6b-4ac4-4981-8976-890864d87cb8`
+
+            fetch(dictionaryUrl)
+            .then(function (response) {
+                
+                return response.json();
+            })
+
+            .then(function (data) {
+                console.log(data);
+
+
+            
+            });
+        
+        };
+
+            getDictionaryData();
