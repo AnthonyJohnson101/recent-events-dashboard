214ab9b9b8ce1c1f29197728ab8d9f30c2b0b302
Beatriz Rivard <beatrizfreire182@gmail.com>
2023-01-17 18:06:19 -0500

    teste

--- assets/script.js
+++ assets/script.js
@@ -40,27 +40,23 @@ function getStockData() {
 //News API setup
 
 //set to selectors
-let thumbnail1
-let title1
-let articleUrl1
+let thumbnail1 = document.querySelector("#thumbnail1")
+let title1 = document.querySelector("#title1")
+let articleUrl1 = document.querySelector("#articleUrl1")
 
-let thumbnail2
-let title2
-let articleUrl2
+let thumbnail2 = document.querySelector("#thumbnail2")
+let title2 = document.querySelector("#title2")
+let articleUrl2 = document.querySelector("#articleUrl2")
 
-let thumbnail3
-let title3
-let articleUrl3
+let thumbnail3 = document.querySelector("#thumbnail3")
+let title3 = document.querySelector("#title3")
+let articleUrl3 = document.querySelector("#articleUrl3")
 
 
 
 function getNewsData() {
 
-    let now = dayjs().format("YYYY-MM-DD")
-
-    let newsTopic = "covid" //make input
-
-    let newsUrl = `https://newsapi.org/v2/top-headlines?q=${newsTopic}&from=${now}&sortBy=popularity&country=us&apiKey=82561c971d50430187557fc656da806a`
+    let newsUrl = `https://gnews.io/api/v4/top-headlines?token=c0ce1376fc76d20830d8473d38ce3ea9&topic=breaking-news&country=us`
         fetch(newsUrl)
                 .then(function (response) {
                     
@@ -70,17 +66,17 @@ function getNewsData() {
                 .then(function (data) {
                     console.log(data);
 
-                    /* thumbnail1.src = data.articles[0].urlToImage
+                    thumbnail1.src = data.articles[0].image
                     title1.textContent = data.articles[0].title
                     articleUrl1.href = data.articles[0].url
 
-                    thumbnail2.src = data.articles[1].urlToImage
+                    thumbnail2.src = data.articles[1].image
                     title2.textContent = data.articles[1].title
                     articleUrl2.href = data.articles[1].url
 
-                    thumbnail3.src = data.articles[2].urlToImage
+                    thumbnail3.src = data.articles[2].image
                     title3.textContent = data.articles[2].title
-                    articleUrl3.href = data.articles[2].url */
+                    articleUrl3.href = data.articles[2].url
 
 
                 });
@@ -88,44 +84,46 @@ function getNewsData() {
 
             getNewsData();
 
-
-
-
 //weather API setup
 let temp
 let icon
-function getWeatherData() {
-    let cityName = "Detroit"
 
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=536f859786a518738a9b20276be37ff1&units=imperial`
+               
+const successCallback = (position) => {
+        console.log(position);
 
-            fetch(weatherUrl)
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=536f859786a518738a9b20276be37ff1&units=imperial`
+
+        fetch(weatherUrl)
             .then(function (response) {
-                
+                        
                 return response.json();
             })
 
             .then(function (data) {
-                console.log(data);
-
-                /* temp = data.list[0].main.temp
-                icon.src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png" */
-
-
+             console.log(data);
+            /* temp = data.list[0].main.temp
+            icon.src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png" */ 
             });
         };
-
-        getWeatherData();
+    
+    const errorCallback = (error) => {
+        console.log(error);
+    };
+    
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);            
 
 //dictionary API setup
 
 let word
-function getDictionaryData() {
-    let word = "purple"
+function randomWordData() {
 
-    let dictionaryUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3f822b6b-4ac4-4981-8976-890864d87cb8`
+    let randomWordUrl = `https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=zzy79quh09ugrg1shgdd2vkt6snleh2tr68h1qp7dma0h9qdy`
 
-            fetch(dictionaryUrl)
+            fetch(randomWordUrl)
             .then(function (response) {
                 
                 return response.json();
@@ -140,4 +138,4 @@ function getDictionaryData() {
         
         };
 
-            getDictionaryData();
+            randomWordData();
